9f5a322984f310b5d03435aec5a5234785b7f7c7
VChet <17050347+VChet@users.noreply.github.com>
2021-06-16 21:31:53 +0500

    feat: add upcoming events section

--- src/App.svelte
+++ src/App.svelte
@@ -36,9 +36,14 @@
     ? events.filter((event) => event.location === selectedLocation)
     : events;
 
+  $: upcomingEvents = filteredEvents.filter((event) =>
+    dayjs().isBefore(dayjs(event.start))
+  );
+
   export let calendar: Calendar | null = null;
   export let selectedDate: Date = new Date();
   export let dayEvents: Array<EventData> = [];
+
   function initCalendar() {
     calendar = new Calendar({
       id: "#calendar",
@@ -63,103 +68,150 @@
 </script>
 
 <main>
-  <div class="calendar-wrapper">
-    <div class="filter" hidden={!locations.length}>
-      <div class="select">
-        <select bind:value={selectedLocation}>
-          <option disabled selected value>Фильтр по городу</option>
-          {#each locations as location}
-            <option value={location}>{location}</option>
-          {/each}
-        </select>
-        <button on:click={() => (selectedLocation = "")}>
-          <span aria-label="Сброс фильтра">❌</span>
-        </button>
-      </div>
-      Результаты: {filteredEvents.length}
-    </div>
-    <div id="calendar" />
-  </div>
-  <div class="day-events">
-    <h2>
-      {dayjs(selectedDate).format("DD.MM.YYYY")}
-      <button on:click={() => calendar.reset(new Date())}>
-        <span aria-label="К сегодняшнему дню">📅</span>
-      </button>
-    </h2>
-    {#each dayEvents as event (event.uid)}
-      <div class="event">
-        <h2>
-          <a href={event.description} rel="noopener">{event.summary}</a>
-        </h2>
-        <div>{event.location}</div>
-        <div>
-          {#if dayjs(event.start).isSame(event.end, "day")}
-            {dayjs(event.start).format("HH:mm")}
-            &mdash;
-            {dayjs(event.end).format("HH:mm")}
-          {:else}
-            {dayjs(event.start).format("DD.MM HH:mm")}
-            &mdash;
-            {dayjs(event.end).format("DD.MM HH:mm")}
-          {/if}
+  <section class="calendar-events">
+    <h1>Календарь событий по фронтенду</h1>
+    <div>
+      <div class="filter" hidden={!locations.length}>
+        <div class="select">
+          <select bind:value={selectedLocation}>
+            <option disabled selected value>Фильтр по городу</option>
+            {#each locations as location}
+              <option value={location}>{location}</option>
+            {/each}
+          </select>
+          <button on:click={() => (selectedLocation = "")}>
+            <span aria-label="Сброс фильтра">❌</span>
+          </button>
         </div>
+        Результаты: {filteredEvents.length}
       </div>
-    {/each}
-  </div>
+      <div id="calendar" />
+    </div>
+    <div class="day-events">
+      <h2>
+        {dayjs(selectedDate).format("DD.MM.YYYY")}
+        <button on:click={() => calendar.reset(new Date())}>
+          <span aria-label="К сегодняшнему дню">📅</span>
+        </button>
+      </h2>
+      {#each dayEvents as event (event.uid)}
+        <div class="event">
+          <h3>
+            <a href={event.description} rel="noopener">{event.summary}</a>
+          </h3>
+          <div>{event.location}</div>
+          <div>
+            {#if dayjs(event.start).isSame(event.end, "day")}
+              {dayjs(event.start).format("HH:mm")}
+              &mdash;
+              {dayjs(event.end).format("HH:mm")}
+            {:else}
+              {dayjs(event.start).format("DD.MM HH:mm")}
+              &mdash;
+              {dayjs(event.end).format("DD.MM HH:mm")}
+            {/if}
+          </div>
+        </div>
+      {/each}
+    </div>
+  </section>
+  <section class="upcoming-events">
+    <h1>Все предстоящие</h1>
+    <div class="events-wrapper">
+      {#each upcomingEvents as event (event.uid)}
+        <div class="event">
+          <h2>{dayjs(event.start).format("DD.MM.YYYY")}</h2>
+          <h3>
+            <a href={event.description} rel="noopener">{event.summary}</a>
+          </h3>
+          <div>{event.location}</div>
+          <div>
+            {#if dayjs(event.start).isSame(event.end, "day")}
+              {dayjs(event.start).format("HH:mm")}
+              &mdash;
+              {dayjs(event.end).format("HH:mm")}
+            {:else}
+              {dayjs(event.start).format("DD.MM HH:mm")}
+              &mdash;
+              {dayjs(event.end).format("DD.MM HH:mm")}
+            {/if}
+          </div>
+        </div>
+      {/each}
+    </div>
+  </section>
 </main>
 
 <style lang="scss">
   main {
+    margin: 40px auto;
+  }
+  h1 {
+    margin: 0 0 16px;
+    text-align: center;
+  }
+  h2 {
+    margin: 0 0 12px;
+    font-size: 28px;
+  }
+  h3 {
+    margin: 0 0 8px;
+  }
+  .calendar-events {
     display: flex;
     flex-wrap: wrap;
     justify-content: center;
-    gap: 40px;
-    margin: 40px auto;
+    gap: 20px 40px;
     text-align: center;
-  }
-  .filter {
-    display: flex;
-    flex-wrap: wrap;
-    gap: 8px;
-    align-items: center;
-    justify-content: space-between;
-    margin-bottom: 20px;
-    .select {
-      flex: 1;
+    h1 {
+      flex-basis: 100%;
+    }
+    .filter {
       display: flex;
+      flex-wrap: wrap;
+      gap: 8px;
       align-items: center;
       justify-content: space-between;
-      > * {
-        border-radius: 0;
-        margin-bottom: 0;
-      }
-      select {
+      margin-bottom: 20px;
+      .select {
         flex: 1;
-      }
-      button {
-        padding: 10px;
-        margin: 0;
+        display: flex;
+        > * {
+          border-radius: 0;
+          margin-bottom: 0;
+        }
+        select {
+          flex: 1;
+        }
+        button {
+          padding: 10px;
+          margin: 0;
+        }
       }
     }
-  }
-  .day-events {
-    width: 300px;
-    h2 {
-      margin: 0 0 16px;
-      font-size: 28px;
+    .day-events {
+      width: 300px;
       button {
         margin: 0;
         padding: 10px;
         border-radius: 0;
         font-size: initial;
       }
-    }
-    .event {
-      font-size: 20px;
-      + .event {
-        margin-top: 20px;
+      .event {
+        font-size: 20px;
+        + .event {
+          margin-top: 20px;
+        }
       }
+    }
+  }
+  .upcoming-events {
+    text-align: center;
+    margin-top: 60px;
+    .events-wrapper {
+      display: grid;
+      grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+      gap: 20px;
     }
   }
 </style>
