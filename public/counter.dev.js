0ae78aa77613bdd37415a1459dac3c175b483d4f
VChet <17050347+VChet@users.noreply.github.com>
2021-09-11 00:45:37 +0500

    chore: add counter.dev analytics

---
+++ public/counter.dev.js
@@ -0,0 +1,15 @@
+if (
+  !sessionStorage.getItem("_swa") &&
+  document.referrer.indexOf(location.protocol + "//" + location.host) !== 0
+) {
+  fetch(
+    "https://counter.dev/track?" +
+      new URLSearchParams({
+        referrer: document.referrer,
+        screen: screen.width + "x" + screen.height,
+        user: "VChet",
+        utcoffset: "5",
+      })
+  );
+}
+sessionStorage.setItem("_swa", "1");
