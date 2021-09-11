cc3bc6384813432b95740cd31d47ea8f9f5f002a
VChet <17050347+VChet@users.noreply.github.com>
2021-09-11 14:44:30 +0500

    chore: update dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,12 @@
-import svelte from 'rollup-plugin-svelte';
 import commonjs from '@rollup/plugin-commonjs';
+import json from "@rollup/plugin-json";
 import resolve from '@rollup/plugin-node-resolve';
-import livereload from 'rollup-plugin-livereload';
-import { terser } from 'rollup-plugin-terser';
-import sveltePreprocess from 'svelte-preprocess';
 import typescript from '@rollup/plugin-typescript';
 import css from 'rollup-plugin-css-only';
+import livereload from 'rollup-plugin-livereload';
+import svelte from 'rollup-plugin-svelte';
+import { terser } from 'rollup-plugin-terser';
+import sveltePreprocess from 'svelte-preprocess';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -64,6 +65,7 @@ export default {
 			sourceMap: true,
 			inlineSources: !production
 		}),
+		json(),
 
 		// In dev mode, call `npm run start` once
 		// the bundle has been generated
